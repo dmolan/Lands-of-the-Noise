70b6b96e79abeeb5b6b73b9ea91fa2fede46c2f6
dmolan <o.dmytruk20@gmail.com>
2022-10-06 14:37:17 +0300

    Added octaves, persistance and lacunarity parametrs to map generator

--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -4,26 +4,50 @@ using UnityEngine;
 
 public static class Noise
 {
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale) 
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, 
+    int octaves, float persistance, float lacunarity) 
     {
         float[,] noiseMap = new float[mapWidth, mapHeight];
 
         if (scale <= 0) scale = 0.0001f;
 
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
         for (int y = 0; y < mapHeight; ++y)
         {
             for (int x = 0; x < mapHeight; ++x)
             {
-                float sampleX = x / scale;
-                float sampleY = y / scale;
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeigh = 0;
+                for (int i = 0; i < octaves; ++i)
+                {
+                    float sampleX = x / scale * frequency;
+                    float sampleY = y / scale * frequency;
 
-                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
-                noiseMap[x, y] = perlinValue;
+                    float perlinValue = 2*Mathf.PerlinNoise(sampleX, sampleY)-1;
+                    noiseHeigh += perlinValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+
+                if (noiseHeigh > maxNoiseHeight) maxNoiseHeight = noiseHeigh;
+                else if (noiseHeigh < minNoiseHeight) minNoiseHeight = noiseHeigh;
+
+                noiseMap[x, y] = noiseHeigh;
+            }
+        }
+
+        for (int y = 0; y < mapHeight; ++y)
+        {
+            for (int x = 0; x < mapHeight; ++x)
+            {
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
             }
         }
         
         return noiseMap;
     }
-
-    
 }
