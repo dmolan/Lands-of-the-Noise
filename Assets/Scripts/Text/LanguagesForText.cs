6715007a905e35133d6af05abc5d7f26a7e65dd1
dmolan <o.dmytruk20@gmail.com>
2022-11-15 23:54:29 +0200

    Started adding multi-language support (done in main and options menu)

---
+++ Assets/Scripts/Text/LanguagesForText.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LanguagesForText : MonoBehaviour
+{
+    public enum Language {English, Ukrainian};
+
+    Language language = Language.Ukrainian;
+
+    public TextMeshProUGUI textMainMenuRun;
+    public TextMeshProUGUI textMainMenuOptions;
+    public TextMeshProUGUI textMainMenuExit;
+
+    public TextMeshProUGUI textSettingsMenuTitle;
+    public TextMeshProUGUI textSettingsMenuVolume;
+    public TextMeshProUGUI textSettingsMenuLanguage;
+
+
+
+    void Start()
+    {
+        if (language == Language.English)
+        {
+            textMainMenuRun.text = "Run";
+            textMainMenuOptions.text = "Options";
+            textMainMenuExit.text = "Exit";
+
+            textSettingsMenuTitle.text = "Settings";
+            textSettingsMenuVolume.text = "Volume";
+            textSettingsMenuLanguage.text = "Language";
+
+        }
+        if (language == Language.Ukrainian)
+        {
+            textMainMenuRun.text = "Запуск";
+            textMainMenuOptions.text = "Опції";
+            textMainMenuExit.text = "Вихід";
+
+            textSettingsMenuTitle.text = "Опції";
+            textSettingsMenuVolume.text = "Гучність";
+            textSettingsMenuLanguage.text = "Мова";
+        }
+    }
+}
