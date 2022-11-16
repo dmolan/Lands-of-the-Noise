7a266890b1993446a96fd11daa030375c583b792
dmolan <o.dmytruk20@gmail.com>
2022-11-16 23:10:51 +0200

    Added language dropdown

--- Assets/Scripts/Text/LanguagesForText.cs
+++ Assets/Scripts/Text/LanguagesForText.cs
@@ -6,42 +6,71 @@ using TMPro;
 
 public class LanguagesForText : MonoBehaviour
 {
-    public enum Language {English, Ukrainian};
-
-    Language language = Language.Ukrainian;
-
     public TextMeshProUGUI textMainMenuRun;
     public TextMeshProUGUI textMainMenuOptions;
     public TextMeshProUGUI textMainMenuExit;
 
-    public TextMeshProUGUI textSettingsMenuTitle;
-    public TextMeshProUGUI textSettingsMenuVolume;
-    public TextMeshProUGUI textSettingsMenuLanguage;
+    public TextMeshProUGUI textSettMenuTitle;
+    public TextMeshProUGUI textSettMenuVolume;
+    public TextMeshProUGUI textSettMenuLanguage;
+    public TMP_Dropdown textSettMenuDrodown;
 
+    public TextMeshProUGUI textAppSeed;
+    public TextMeshProUGUI textAppLacunarity;
+    public TextMeshProUGUI textAppPersistance;
+    public TextMeshProUGUI textAppScale;
+    public TextMeshProUGUI textAppOffsetSpeed;
 
+    private void changeLanguageToUkr()
+    {
+        textMainMenuRun.text = "Запуск";
+        textMainMenuOptions.text = "Опції";
+        textMainMenuExit.text = "Вихід";
+
+        textSettMenuTitle.text = "Опції";
+        textSettMenuVolume.text = "Гучність";
+        textSettMenuLanguage.text = "Мова";
+        textSettMenuDrodown.options.Clear();
+        textSettMenuDrodown.options.Add(new TMP_Dropdown.OptionData() {text = "Українська"});
+        textSettMenuDrodown.options.Add(new TMP_Dropdown.OptionData() {text = "Англійська"}); 
+        textSettMenuDrodown.captionText.text = "Українська";
+
+        textAppSeed.text = "Зерно";
+        textAppLacunarity.text = "Лакунарність";
+        textAppPersistance.text = "Наполегливість";
+        textAppScale.text = "Масштаб";
+        textAppOffsetSpeed.text = "Швидкість зсуву";
+    }
+
+    private void changeLanguageToEng()
+    {
+        textMainMenuRun.text = "Run";
+        textMainMenuOptions.text = "Options";
+        textMainMenuExit.text = "Exit";
+
+        textSettMenuTitle.text = "Settings";
+        textSettMenuVolume.text = "Volume";
+        textSettMenuLanguage.text = "Language";
+        textSettMenuDrodown.options.Clear();
+        textSettMenuDrodown.options.Add(new TMP_Dropdown.OptionData() {text = "Ukrainian"});
+        textSettMenuDrodown.options.Add(new TMP_Dropdown.OptionData() {text = "English"});
+        textSettMenuDrodown.captionText.text = "English";
+
+        textAppSeed.text = "Seed";
+        textAppLacunarity.text = "Lacunarity";
+        textAppPersistance.text = "Persistance";
+        textAppScale.text = "Scale";
+        textAppOffsetSpeed.text = "Offset Speed";
+    }
+
+    public void changeLanguage(int val)
+    {
+        if (val == 0) changeLanguageToUkr();
+        else if (val == 1) changeLanguageToEng();
+    }
 
     void Start()
     {
-        if (language == Language.English)
-        {
-            textMainMenuRun.text = "Run";
-            textMainMenuOptions.text = "Options";
-            textMainMenuExit.text = "Exit";
-
-            textSettingsMenuTitle.text = "Settings";
-            textSettingsMenuVolume.text = "Volume";
-            textSettingsMenuLanguage.text = "Language";
-
-        }
-        if (language == Language.Ukrainian)
-        {
-            textMainMenuRun.text = "Запуск";
-            textMainMenuOptions.text = "Опції";
-            textMainMenuExit.text = "Вихід";
-
-            textSettingsMenuTitle.text = "Опції";
-            textSettingsMenuVolume.text = "Гучність";
-            textSettingsMenuLanguage.text = "Мова";
-        }
+        changeLanguageToUkr();
     }
 }
