9476086a458af24ae7a6a43ffd579cf5b8704794
dmolan <o.dmytruk20@gmail.com>
2022-11-17 10:19:00 +0200

    Added save/load file, but no real functionality yet

---
+++ Assets/Scripts/SaveLoad/SaveMap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SaveMap : MonoBehaviour
+{
+    public void saveCurrentMap()
+    {
+        string savePath = EditorUtility.SaveFilePanel("Save map as TXT", "", "mapSave" + ".txt", "txt");
+        if (savePath.Length != 0)
+        {
+            System.IO.File.WriteAllText(savePath, "Hello hello hey!");
+        }
+    }
+    public void loadMap()
+    {
+        string pathLoad = EditorUtility.OpenFilePanel("Load a map from TXT", "", "txt");
+        if (pathLoad.Length != 0)
+        {
+            System.IO.File.WriteAllText(pathLoad, "No no no no :)");
+        }
+
+    }
+}
