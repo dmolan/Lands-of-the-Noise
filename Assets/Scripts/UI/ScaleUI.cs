5b3d4d2f51d4f0957faaf99fa162966fe2921d6c
dmolan <o.dmytruk20@gmail.com>
2022-11-23 00:23:32 +0200

    Added "How To Use" canvas and buttons for movement inside it

--- Assets/Scripts/UI/ScaleUI.cs
+++ Assets/Scripts/UI/ScaleUI.cs
@@ -9,14 +9,18 @@ public class ScaleUI : MonoBehaviour
     public Canvas canvasMenu;
     public Canvas canvasApp;
     public Canvas canvasPreferences;
+    public Canvas canvasHowUse;
 
-    private float defMenuScale = 0.6f;
-    private float defAppScale = 0.9f;
-    private float defPrefScale = 0.65f;
+    private float defMenuScale = 0.4f;
+    private float defAppScale = 0.7f;
+    private float defPrefScale = 0.45f;
+    private float defHowUseScale = 0.45f;
 
     private float menuScale;
     private float appScale;
     private float prefScale;
+    private float howUseScale;
+
 
 
 
@@ -25,6 +29,7 @@ public class ScaleUI : MonoBehaviour
         menuScale = defMenuScale;
         appScale = defAppScale;
         prefScale = defPrefScale;
+        howUseScale = defHowUseScale;
     }
 
     void Update()
@@ -32,33 +37,38 @@ public class ScaleUI : MonoBehaviour
         if (canvasMenu.scaleFactor != menuScale) canvasMenu.scaleFactor = menuScale;
         if (canvasApp.scaleFactor != appScale) canvasApp.scaleFactor = appScale;
         if (canvasPreferences.scaleFactor != prefScale) canvasPreferences.scaleFactor = prefScale;
+        if (canvasHowUse.scaleFactor != howUseScale) canvasHowUse.scaleFactor = howUseScale;
     }
 
     public void changeCanvasScale(int val)
     {
-        if (val == 0) 
+        if (val == 0)
         {
-            menuScale = defMenuScale*(3/4f);
-            appScale = defAppScale*(3/4f);
-            prefScale = defPrefScale*(3/4f);
+            menuScale = defMenuScale * (3/4f);
+            appScale = defAppScale * (3/4f);
+            prefScale = defPrefScale * (3/4f);
+            howUseScale = defHowUseScale * (3/4f);
         }
-        else if (val == 1) 
+        else if (val == 1)
         {
             menuScale = defMenuScale;
             appScale = defAppScale;
             prefScale = defPrefScale;
+            howUseScale = defHowUseScale;
         }
-        else if (val == 2) 
+        else if (val == 2)
         {
             menuScale = defMenuScale * (5/4f);
             appScale = defAppScale * (5/4f);
             prefScale = defPrefScale * (5/4f);
+            howUseScale = defHowUseScale * (5/4f);
         }
-        else if (val == 3) 
+        else if (val == 3)
         {
             menuScale = defMenuScale * (3/2f);
             appScale = defAppScale * (3/2f);
             prefScale = defPrefScale * (3/2f);
+            howUseScale = defHowUseScale*(3/2f);
         }
     }
 }
